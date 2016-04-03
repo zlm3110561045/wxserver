f98fad28eeae66d936cbc30bb36f066c3304d6ed
zhangliming <zlm3110561045@163.com>
2016-04-03 17:57:22 +0800

    update for test
    for test

--- webapp/src/main/java/cn/saymagic/weixin/server/Config.java
+++ webapp/src/main/java/cn/saymagic/weixin/server/Config.java
@@ -3,9 +3,9 @@ package cn.saymagic.weixin.server;
 public class Config {
 
 	/** 微信公众号的token，用于验证之用*/
-	public static final String TOKEN = "";
+	public static final String TOKEN = "05a5e7253359db727fa18ef1e9e965e0";
 	
 	/** 图灵机器人的APPKEY，可在图灵官方申请*/
-	public static final String APIKEY = ""; 
+	public static final String APIKEY = "933628f7dea28257addc75466438e864"; 
 	
 }
